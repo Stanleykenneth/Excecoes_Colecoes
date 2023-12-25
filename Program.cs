f2d300e5abbea27e405139ea37ce14cb6bfb43a1
StanleyKenneth <kstanley_bass@hotmail.com>
2023-12-25 20:19:59 -0300

    [Programs] - Trabalhando com filas e pilhas

--- Program.cs
+++ Program.cs
@@ -1,29 +1,74 @@
-﻿using Exemplos.Models;
+﻿using Excecoes_Colecoes.Models;
+using Exemplos.Models;
 using System.Globalization;
 
 
 
-string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+//TRABALHANDO COM PILHAS
 
-foreach(string linha in linhas){
+// Stack<int> pilha = new Stack<int>();
 
-    Console.WriteLine(linha);
-}
+// pilha.Push(4);
+// pilha.Push(6);
+// pilha.Push(8);
+// pilha.Push(10);
+
+// foreach(int item in pilha){
+
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Remeovendo o elemento no topo: {pilha.Pop()}");
+// pilha.Push(20);
+
+// foreach(int item in pilha){
+
+//     Console.WriteLine(item);
+// }
+
+
+// TRABALHANDO COM FILAS
+// Queue<int> fila = new Queue<int>();
+
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
+
+// foreach(int item in fila){
+
+//     Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+
+// foreach(int item in fila){
+
+//     Console.WriteLine(item);
+// }
 
 
 
 
 
 
-// Pessoa p1 = new Pessoa("Fulano", "de Tal");
 
-// Pessoa p2 = new Pessoa("Beltrano", "da Silva");
 
-// Curso cursoDeCsharp = new Curso();
-// cursoDeCsharp.Nome = "Curso de CShrap";
-// cursoDeCsharp.Alunos = new List<Pessoa>();
+// new ExemploExcecao().Metodo1();
 
-// cursoDeCsharp.Adicionaraluno(p1);
-// cursoDeCsharp.Adicionaraluno(p2);
-// cursoDeCsharp.ListarAlunos();
+// try{
+
+//     string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+
+//     foreach(string linha in linhas){
+
+//     Console.WriteLine(linha);
+//     }
+
+// }
+// catch (Exception ex){ 
+    
+//     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+// }
 
